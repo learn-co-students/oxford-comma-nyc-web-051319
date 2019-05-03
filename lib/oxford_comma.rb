36809f78b30a0960dd50a72c3eefe1925c36c060
William Bainton <baintonw@gmail.com>
2019-05-03 23:25:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif
+    array.length == 2
+    array.join(" and ")
+  else
+    first = array[0...-1].join(", ")
+    last = array[-1].to_s
+    "#{first}" << ", and #{last}"
+  end
 end
